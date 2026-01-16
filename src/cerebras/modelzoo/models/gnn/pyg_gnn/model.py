f5eb1481732b2c89b7e9f5374beda44f4405aaba
watanuki <watanuki@ac2.scrc.iir.isct.ac.jp>
2026-01-16 22:11:33 +0900

    fix: adjust PyG model to cszoo one

--- src/cerebras/modelzoo/models/gnn/pyg_gnn/model.py
+++ src/cerebras/modelzoo/models/gnn/pyg_gnn/model.py
@@ -1,6 +1,30 @@
 import torch
 from torch_geometric.nn.models import GraphSAGE
+import torch.nn.functional as F
 from .cagnet_model import CagnetSAGE
+
+class GraphSAGEWrapper(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
+        super().__init__()
+        self.gnn = GraphSAGE(
+            in_channels=in_channels,
+            hidden_channels=hidden_channels,
+            out_channels=hidden_channels, # Backbone outputs hidden_dim
+            num_layers=num_layers,
+            dropout=dropout,
+            act="relu",
+            norm=None,
+            jk=None
+        )
+        self.classifier = torch.nn.Linear(hidden_channels, out_channels)
+        self.dropout_p = dropout
+
+    def forward(self, x, edge_index, batch_size=None, **kwargs):
+        # PyG GraphSAGE doesn't require batch_size for standard forward
+        x = self.gnn(x, edge_index)
+        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        x = self.classifier(x)
+        return x
 
 def get_model(config, args=None, num_nodes=None):
     m = config["trainer"]["init"]["model"]
@@ -39,15 +63,13 @@ def get_model(config, args=None, num_nodes=None):
         )
     else:
         # Use native PyG GraphSAGE for efficiency in single-process / standard DDP
-        model = GraphSAGE(
+        # We use a wrapper to ensure a separate classifier head, matching CSZoo reference architecture
+        model = GraphSAGEWrapper(
             in_channels=m["n_feat"],
             hidden_channels=m["graphsage_hidden_dim"],
             out_channels=m["n_class"],
-            num_layers=2,
-            dropout=m["graphsage_dropout"],
-            act="relu",
-            norm=None, # CagnetSAGE didn't use BN/LN, just row norm on adj (handled by SAGEConv)
-            jk=None
+            num_layers=m["graphsage_num_layers"],
+            dropout=m["graphsage_dropout"]
         )
 
     return model
